48245c86d73673f1aca0d3c6a8475e3c293d8fdf
zun_e <zun_e@zun-es-Mac-Pro.local>
2022-08-15 20:05:37 +0900

    bottomNavigation 제작

---
+++ component/SettingsPage.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import {
+    SafeAreaView,
+    Text,
+    View,
+    StyleSheet,
+    Image,
+    TouchableOpacity,
+    ScrollView,
+} from 'react-native'
+import * as Progress from 'react-native-progress'
+export const SettingsPage = ({ navigation }) => {
+    return (
+        <>
+            <SafeAreaView style={styles.mainContainer}>
+                <View style={{ marginLeft: 20, marginBottom: 30 }}>
+                    <Text style={styles.mainText}>설정</Text>
+                </View>
+                <ScrollView
+                    showsVerticalScrollIndicator={false}
+                    showsHorizontalScrollIndicator={false}
+                >
+                    <TouchableOpacity style={styles.card}>
+                        <Image
+                            source={require('../assets/image/room.png')}
+                            style={styles.cardImg}
+                        />
+                        <Text style={styles.mainText}>방 구조 설명하기</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.card}>
+                        <Image
+                            source={require('../assets/image/schedule.png')}
+                            style={styles.cardImg}
+                        />
+                        <Text style={styles.mainText}>일정 짜기</Text>
+                    </TouchableOpacity>
+                </ScrollView>
+            </SafeAreaView>
+        </>
+    )
+}
+
+const styles = StyleSheet.create({
+    mainContainer: {
+        flex: 1,
+    },
+    mainText: {
+        fontFamily: 'GothicThin',
+        fontSize: 40,
+        color: 'black',
+    },
+    cardText: {
+        fontFamily: 'GothicLight',
+        fontSize: 50,
+        color: '#f2f1f7',
+        marginTop: 20,
+    },
+    card: {
+        borderRadius: 10,
+        width: '80%',
+        height: 300,
+        backgroundColor: '#f2f5f9',
+        shadowColor: '#3b4054',
+        shadowOffset: { width: 2, height: 2 },
+        shadowOpacity: 0.1,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        marginTop: 10,
+        marginBottom: 10,
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    cardImg: {
+        width: 150,
+        height: 150,
+    },
+})
--- navigation/BottomStack.jsx
+++ navigation/BottomStack.jsx
@@ -1,51 +1,104 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Image, StyleSheet } from "react-native";
-import { Main } from "../component/Main";
-const Tab = createBottomTabNavigator();
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Image, Settings, StyleSheet } from 'react-native'
+import { Main } from '../component/Main'
+import { SettingsPage } from '../component/SettingsPage'
+import { Statistic } from '../component/Statistic'
+const Tab = createBottomTabNavigator()
 const BottomStack = () => {
-  return (
-    <Tab.Navigator
-      initialRouteName="Home"
-      screenOptions={({ route }) => ({
-        tabBarStyle: {
-          borderTopColor: "#3c3f44",
-          backgroundColor: "#EEEDED",
-          height: 80,
-          borderTopWidth: 0.2,
-          paddingTop: 10,
-        },
-        headerShown: true,
-        headerTransparent: true,
-        tabBarActiveTintColor: "white",
-        tabBarInactiveTintColor: "#71778d",
-      })}
-    >
-      <Tab.Screen
-        name="Home"
-        options={{
-          title: "",
-          tabBarIcon: ({ focused }) => (
-            <Image
-              style={styles.tabIcon}
-              source={
-                focused
-                  ? require("../assets/image/home.png")
-                  : require("../assets/image/homeNotFocus.png")
-              }
-            />
-          ),
-        }}
-        component={Main}
-      ></Tab.Screen>
-    </Tab.Navigator>
-  );
-};
+    return (
+        <Tab.Navigator
+            initialRouteName="Home"
+            screenOptions={({ route }) => ({
+                tabBarStyle: {
+                    borderTopColor: '#3c3f44',
+                    backgroundColor: '#EEEDED',
+                    height: 80,
+                    borderTopWidth: 0.2,
+                    paddingTop: 10,
+                },
+                headerShown: true,
+                headerTransparent: true,
+                tabBarActiveTintColor: 'white',
+                tabBarInactiveTintColor: '#71778d',
+            })}
+        >
+            <Tab.Screen
+                name="Home"
+                options={{
+                    title: '',
+                    tabBarIcon: ({ focused }) => (
+                        <Image
+                            style={styles.tabIcon}
+                            source={
+                                focused
+                                    ? require('../assets/image/home.png')
+                                    : require('../assets/image/homeNotFocus.png')
+                            }
+                        />
+                    ),
+                }}
+                component={Main}
+            ></Tab.Screen>
+            <Tab.Screen
+                name="Statistic"
+                options={{
+                    title: '',
+                    tabBarIcon: ({ focused }) => (
+                        <Image
+                            style={styles.tabIcon}
+                            source={
+                                focused
+                                    ? require('../assets/image/statistic.png')
+                                    : require('../assets/image/statisticNotFocus.png')
+                            }
+                        />
+                    ),
+                }}
+                component={Statistic}
+            ></Tab.Screen>
+            <Tab.Screen
+                name="Community"
+                options={{
+                    title: '',
+                    tabBarIcon: ({ focused }) => (
+                        <Image
+                            style={styles.tabIcon}
+                            source={
+                                focused
+                                    ? require('../assets/image/community.png')
+                                    : require('../assets/image/communityNotFocus.png')
+                            }
+                        />
+                    ),
+                }}
+                component={Main}
+            ></Tab.Screen>
+            <Tab.Screen
+                name="SettingsPage"
+                options={{
+                    title: '',
+                    tabBarIcon: ({ focused }) => (
+                        <Image
+                            style={styles.tabIcon}
+                            source={
+                                focused
+                                    ? require('../assets/image/settings.png')
+                                    : require('../assets/image/settingsNotFocus.png')
+                            }
+                        />
+                    ),
+                }}
+                component={SettingsPage}
+            ></Tab.Screen>
+        </Tab.Navigator>
+    )
+}
 const styles = StyleSheet.create({
-  tabIcon: {
-    width: 25,
-    height: 25,
-  },
-});
-export default BottomStack;
+    tabIcon: {
+        width: 25,
+        height: 25,
+    },
+})
+export default BottomStack
