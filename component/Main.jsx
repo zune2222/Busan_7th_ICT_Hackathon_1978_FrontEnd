eb3625aa35a662acb090c573cdf9066ab0f931e4
zun_e <zun_e@zun-es-Mac-Pro.local>
2022-08-15 19:22:45 +0900

    메인화면 프로그램 진행도 & 현재 시간

--- component/Main.jsx
+++ component/Main.jsx
@@ -8,12 +8,28 @@ import {
     TouchableOpacity,
     ScrollView,
 } from 'react-native'
+import * as Progress from 'react-native-progress'
 export const Main = ({ navigation }) => {
+    const hours = new Date().getHours()
+    const min = new Date().getMinutes()
     return (
         <>
             <SafeAreaView style={styles.mainContainer}>
-                <View style={{ marginLeft: 20, marginTop: 0 }}>
-                    <Text style={styles.mainText}>갓생하고살자</Text>
+                <View style={{ marginLeft: 20, marginBottom: 30 }}>
+                    <Text style={styles.mainText}>
+                        {hours}:{min}
+                    </Text>
+                    <Text style={styles.mainText}>책상에서 코딩하기</Text>
+                </View>
+                <View style={styles.card}>
+                    <TouchableOpacity>
+                        <Progress.Circle
+                            size={250}
+                            progress={0.3}
+                            showsText={true}
+                            thickness={4}
+                        />
+                    </TouchableOpacity>
                 </View>
             </SafeAreaView>
         </>
@@ -39,10 +55,10 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         width: '80%',
         height: 300,
-        backgroundColor: '#3b4054',
+        backgroundColor: '#f2f5f9',
         shadowColor: '#3b4054',
         shadowOffset: { width: 2, height: 2 },
-        shadowOpacity: 0.3,
+        shadowOpacity: 0.1,
         marginLeft: 'auto',
         marginRight: 'auto',
         marginTop: 10,
